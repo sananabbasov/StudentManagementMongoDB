411bd75e2cfb423878cfb717b3bd1dbbdedffc4d
sananabbasov <sanan_abbasov@list.ru>
2022-02-26 15:52:53 +0400

    Add project files.

---
+++ StudentManagement/Models/IStudentStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagement.Models
+{
+    public interface IStudentStoreDatabaseSettings
+    {
+        public string StudentCoursesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ StudentManagement/Models/StudentStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagement.Models
+{
+    public class StudentStoreDatabaseSettings : IStudentStoreDatabaseSettings
+    {
+        public string StudentCoursesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
